041011023cf839e38cf76c171be31e3f603316b2
GopalAror <punjabijanu586@gmail.com>
2023-11-17 11:23:10 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,22 @@ menu.addEventListener("click",function(){
   mobile.classList.toggle("view");
   document.querySelector("body").classList.toggle("overflow_hidden")
 })
+
+// back-to-top
+function scrollBtn() {
+  window.scrollTo(0, 0);
+}
+window.addEventListener("scroll", function () {
+  let topbtn = document.getElementById("topbtn");
+  if (window.scrollY > 300) {
+      topbtn.style.display = 'block';
+  }
+  else{
+      topbtn.style.display = 'none';
+  }
+})
+// preloder
+setTimeout(() => {
+  document.getElementById("preloder").classList.add("d-none");
+  document.body.classList.remove("overflow-hidden");
+}, 2000);
